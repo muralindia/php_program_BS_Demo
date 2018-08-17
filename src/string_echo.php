75b12e7ba8b3151866d6a476029c1ad55603bbfa
Murali Dhandapani <muralindia@gmail.com>
2018-08-17 04:19:57 +0000

    2 more programs - comments_string_echo.php and php_echo.php

---
+++ src/string_echo.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+// This is a single-line comment
+
+# This is also a single-line comment
+
+/*
+This is a multiple-lines comment block
+that spans over multiple
+lines
+*/
+
+// You can also use comments to leave out parts of a code line
+$x = 5 /* + 15 */ + 5;
+echo $x;
+
+$txt1 = "Learn PHP";
+$txt2 = "W3Schools.com";
+$x = 5;
+$y = 4;
+
+echo "<h2>" . $txt1 . "</h2>";
+echo "Study PHP at " . $txt2 . "<br>";
+echo $x + $y;
+
+
+
+?>
+
+</body>
+</html>
